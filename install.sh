3955a25007c60c76e3dc265698ef777bde5bec0a
Renan Decamps <renan.decamps@gmail.com>
2019-11-05 17:38:26 +0100

    feat(vscode): add settings.json config and linking

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+DOTFILES_FOLDER=`pwd`
+
 clear
 echo "Trying to do some things here and here..."
+echo "Using $DOTFILES_FOLDER"
 
 ## VSCode config files
-echo "Symlinking config to the $HOME/Library/Application Support/Code/User/"
+VSCODE_CONFIG_PATH="$HOME/Library/Application Support/Code/User"
+echo "Symlinking config to the $VSCODE_CONFIG_PATH"
+ln -sf "$DOTFILES_FOLDER/vscode/settings.json" "$VSCODE_CONFIG_PATH/settings.json"
