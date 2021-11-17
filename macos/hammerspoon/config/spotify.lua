a971e981ed9c853568ee57e9c8e5e97f0337dbed
Renan <renan.decamps@gmail.com>
2021-11-17 10:04:52 +0100

    chore: remove hammerspoon (now using raycast

--- macos/hammerspoon/config/spotify.lua
+++
@@ -1,3 +0,0 @@
-hs.hotkey.bind(hyper, "right", function() hs.spotify.next() end)
-hs.hotkey.bind(hyper, "left", function() hs.spotify.previous() end)
-hs.hotkey.bind(hyper, "up", function() hs.spotify.playpause() end)
